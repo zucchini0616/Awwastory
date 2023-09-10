529d919e6f4ae171dca97a2908a03a9cb641e23f
He Zexi <hezexi0616@gmail.com>
2023-09-10 15:34:28 +0800

    move file out of public

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  // Point to the new location of your index.html
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const port = 8080;
 app.listen(port, () => {
-  console.log(`Server running on http://http://13.229.232.201:${port}`);
+  console.log(`Server running on http://13.229.232.201:${port}`);
 });
